bc89147999327daaae231b7d8b807d86189bd050
Damian Agasi <agasidamian@gmail.com>
2020-05-21 17:15:22 -0300

    'sgundo'

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-
+var movLeftCustomer = 1;
 document.addEventListener('DOMContentLoaded', function() {
     var index = 1;
     setActive('navInicio', 'inicio');
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
         if(index > 2){
             index = 1;
         }}, 10000);
+    setInterval(a => {
+        movCustomers();
+    }, 2000)
  }, false);
 
 
@@ -23,23 +26,20 @@ function setActive(name){
 }
 
 
-    function changeCompanyImg(index){
-        console.log(index)
-        console.log('company-img')
-        var sec = document.getElementById('company-img');
-        var bg = sec.style;
-        switch(index){
-            case 1:
-                sec.src= "images/servicios.gif";
-                break;
-            case 2:
-                sec.src = "images/empresa.png";
-                break;
-        }
-        
-    
-}
 
+
+function movCustomers(){
+    var div = document.getElementById('customers');
+    div.scrollLeft += div.offsetWidth * 0.2;
+    console.log(div.scrollLeft,div.offsetWidth)
+        if(div.scrollLeft >= div.offsetWidth)
+        {
+            div.scrollLeft = 0;
+        }
+  
+
+
+}
 
 function changeBanner(index){
     console.log(index)
@@ -77,8 +77,18 @@ window.onscroll = function() {
   if(window.scrollY >= 500){
       setAnimation(1);
   }
-  if(window.scrollY >= 2000){
+  
+  if(window.scrollY >= 1100){
     setAnimation(2);
+} 
+if(window.scrollY >= 1500){
+  setAnimation(3);
+}
+  if(window.scrollY >= 2000){
+    setAnimation(4);
+}
+if(window.scrollY >= 2700){
+    setAnimation(5);
 }
   };
 
@@ -107,25 +117,55 @@ window.onscroll = function() {
               p3.className = '';
               break;
         case 2:
-            var li1 = document.getElementById('lii1'),
-            li2 = document.getElementById('lii2'),
-            li3 = document.getElementById('li12'),
-            li4 = document.getElementById('lii4'),
-            li5 = document.getElementById('lii5'),
-            li6 = document.getElementById('lii6');
-            li1.style = 'transform: translate(0%)';
-            setTimeout(() => {
-                li2.style = 'transform: translate(5%)';
-            }, 500);
-            setTimeout(() => {
-                li4.style = 'transform: translate(10%)';
-            }, 1000);
-            setTimeout(() => {;
-                li5.style = 'transform: translate(15%)';
-            }, 1500);
-            setTimeout(() => {;
-                li6.style = 'transform: translate(20%)';
-            }, 2000);
+            var t1 = document.getElementById('text1');
+             t1.className = ''; 
+            break
+            case 3:  var t2 = document.getElementById('text2');
+            t2.className = '';
             break;
+        // case 4:
+        //     var li1 = document.getElementById('lii1'),
+        //     li2 = document.getElementById('lii2'),
+        //     li3 = document.getElementById('li12'),
+        //     li4 = document.getElementById('lii4'),
+        //     li5 = document.getElementById('lii5'),
+        //     li6 = document.getElementById('lii6');
+        //     li1.style = 'transform: translate(0%)';
+        //     setTimeout(() => {
+        //         li2.style = 'transform: translate(5%)';
+        //     }, 500);
+        //     setTimeout(() => {
+        //         li4.style = 'transform: translate(10%)';
+        //     }, 1000);
+        //     setTimeout(() => {;
+        //         li5.style = 'transform: translate(15%)';
+        //     }, 1500);
+        //     setTimeout(() => {;
+        //         li6.style = 'transform: translate(20%)';
+        //     }, 2000);
+        //     break;
+
+            
+            case 3:  var t2 = document.getElementById('text2');
+            t2.className = '';
+            break;
+            case 5:
+                var step1 = document.getElementById('step1'), 
+                step2 = document.getElementById('step2'),
+                step3 = document.getElementById('step3'),
+                step4 = document.getElementById('step4'),
+                hr = document.getElementById('hrStep');
+                hr.className = ' widthhr ';
+                step1.className = '';
+                setTimeout(() => {
+                    step2.className = '';
+                }, 500);
+                setTimeout(() => {
+                    step3.className = '';
+                }, 500 + 700 + 400);
+                setTimeout(() => {
+                    step4.className = '';
+                }, 500 + 700 + 400 + 500 + 400);
+                break;
       }
   }
